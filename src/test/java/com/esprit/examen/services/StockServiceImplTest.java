f3ef965aa81b8ec59cfa5340cedd49d39a25ec51
mahdi ayari <ayarii.mahdii@gmail.com>
2023-03-23 15:21:18 +0100

    tests

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,52 +1,58 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
-import static org.junit.Assert.*;
-import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.esprit.examen.entities.Stock;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import com.esprit.examen.entities.Stock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Slf4j
 public class StockServiceImplTest {
-	@Autowired
-	IStockService stockService;
-	
-	@Test
-	public void testAddStock() {
-	//	List<Stock> stocks = stockService.retrieveAllStocks();
-	//	int expected=stocks.size();
-		Stock s = new Stock("stock test",10,100);
-		Stock savedStock= stockService.addStock(s);
-		
-	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
-		assertNotNull(savedStock.getLibelleStock());
-		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
-	@Test
-	public void testAddStockOptimized() {
+    @Autowired
+    IStockService stockService;
 
-		Stock s = new Stock("stock test",10,100);
-		Stock savedStock= stockService.addStock(s);
-		assertNotNull(savedStock.getIdStock());
-		assertSame(10, savedStock.getQte());
-		assertTrue(savedStock.getQteMin()>0);
-		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
-	@Test
-	public void testDeleteStock() {
-		Stock s = new Stock("stock test",30,60);
-		Stock savedStock= stockService.addStock(s);
-		stockService.deleteStock(savedStock.getIdStock());
-		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
-	}
+    @Test
+    public void testAddStock() {
+        Stock s = new Stock("test",545,98);
+        Stock savedStock= stockService.addStock(s);
+        assertSame(98, savedStock.getQteMin());
+        assertTrue( savedStock.getQte()==545);
+        assertNotNull(savedStock.getLibelleStock());
+        stockService.deleteStock(savedStock.getIdStock());
+    }
+
+    @Test
+    public void testUpdatestock() {
+
+        Stock s = new Stock("test2",78,98);
+        Stock s2= stockService.addStock(s);
+        assertNotNull(s2.getIdStock());
+        Stock stock= stockService.retrieveStock(s2.getIdStock());
+        assertTrue(stock.getQteMin()>10);
+        stockService.deleteStock(s2.getIdStock());
+    }
+
+    @Test
+    public void testDeleteStock() {
+        Stock s = new Stock("test3",44,511);
+        Stock savedStock= stockService.addStock(s);
+        stockService.deleteStock(savedStock.getIdStock());
+        assertNull(stockService.retrieveStock(savedStock.getIdStock()));
+    }
+    @Test
+    public void testretrieveallstocks() {
+        int expected = stockService.retrieveAllStocks().size();
+        Stock s = new Stock("test4",68,695);
+        Stock savedStock= stockService.addStock(s);
+        assertEquals(expected + 1, stockService.retrieveAllStocks().size());
+        stockService.deleteStock(savedStock.getIdStock());
+    }
 
 }
-*/
+
